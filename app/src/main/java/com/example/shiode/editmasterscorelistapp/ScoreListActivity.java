7bab51260d12d8415051985da0febed0a21d3d2a
Kenshi Shiode <atemu1212@yahoo.co.jp>
2019-02-09 15:37:14 +0900

    init

---
+++ app/src/main/java/com/example/shiode/editmasterscorelistapp/ScoreListActivity.java
@@ -0,0 +1,13 @@
+package com.example.shiode.editmasterscorelistapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScoreListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_list);
+    }
+}
---
+++ app/src/main/java/com/example/shiode/editmasterscorelistapp/ScoreListViewModel.java
@@ -0,0 +1,6 @@
+package com.example.shiode.editmasterscorelistapp;
+
+import android.arch.lifecycle.ViewModel;
+
+public class ScoreListViewModel extends ViewModel {
+}
